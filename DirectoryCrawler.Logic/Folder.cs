579464ce9d2c26cea1e81859021139a075de01b0
Pennings,Wouter W.E.M <465288@student.fontys.nl>
2021-06-21 16:51:04 +0200

    Refactor done

---
+++ DirectoryCrawler.Logic/Folder.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DirectoryCrawler.Logic
+{
+    public class Folder
+    {
+        private string _name;
+
+        public Folder(string name)
+        {
+            _name = name;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
