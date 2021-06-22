6b840b2280dc8bad65801abdc433827ab78bfcd3
Pennings,Wouter W.E.M <465288@student.fontys.nl>
2021-06-22 22:54:20 +0200

    error handling server

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -20,11 +20,26 @@ wss.on('connection', (ws: any) => {
 
     ws.on('message', (message: string) => {
         console.log('[server] Received message: %s', message);
-        wss.clients.forEach(function each(client: any){
-            if(client.readyState === WebSocket.OPEN){
-                const json: any = JSON.parse(message);
-                client.send(json[0] + json[1] + json[2]);
-            }
-        });
+        const json2 = GetJSON(message);
+        if(json2 !== false){
+            wss.clients.forEach(function each(client: any){
+                if(client.readyState === WebSocket.OPEN){
+                    const json: any = JSON.parse(message);
+                    if(json.hasOwnProperty('files')) client.send(json.files);
+                    else client.send('{"error":"Key: files, was missing."}');
+                }
+            });
+        }
+        else ws.send('{"error":"JSON was invalid."}');
     });
 });
+
+function GetJSON(jsonStr: string){
+    try{
+        return JSON.parse(jsonStr);
+    }
+    catch(error){
+        console.log(error);
+        return false;
+    }
+}
